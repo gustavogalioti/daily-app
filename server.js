259a2cf53fcca4f7d527e3e089912c5b86628f35
gustavogalioti <gustavowolkerz@gmail.com>
2026-06-02 18:26:23 -0300

    feat: registrar rota /api/dailypoke

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ process.on('uncaughtException', (err) => {
     ['/api/testimonials', './testimonials'], ['/api/communities', './communities'],
     ['/api/user-notifications', './user_notifications'],
     ['/api/agenda', './agenda'],
+    ['/api/dailypoke', './dailypoke'],
     ['/api/achievements', './achievements'], ['/api/pedro', './pedro'],
     ['/api/daily-questions', './daily_questions'], ['/api/geo', './geo'],
     ['/api/map-points', './map_points'], ['/api/events', './events'],
