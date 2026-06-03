cae10ff7ac7531ce0802e70e15ee1ed8a985e109
gustavogalioti <gustavowolkerz@gmail.com>
2026-06-03 17:29:54 -0300

    feat: rota /api/agora-chat

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ process.on('uncaughtException', (err) => {
     ['/api/agenda', './agenda'],
     ['/api/dailypoke', './dailypoke'],
     ['/api/catrunner', './catrunner'],
+    ['/api/agora-chat', './agora_chat'],
     ['/api/achievements', './achievements'], ['/api/pedro', './pedro'],
     ['/api/daily-questions', './daily_questions'], ['/api/geo', './geo'],
     ['/api/map-points', './map_points'], ['/api/events', './events'],
